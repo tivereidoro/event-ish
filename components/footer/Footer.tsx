9363984f7bef5cf045511156aa0508680f0a8156
tivereidoro <tivereidoro@yahoo.com>
2024-09-15 15:31:28 +0100

    Started backend and db

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import logo from "@/public/assets/images/logo.svg";
 
 export default function Footer() {
     return (
-        <div>
-            <h3>Footer goes here</h3>
-        </div>
+        <footer className='border-t'>
+            <div className='flex-center wrapper flex flex-between flex-col gap-4 p-5 text-center sm:flex-row'>
+                <Link href="/" className='w-36'>
+                    <Image src={logo} width={128} height={38} alt='Event-ish logo' />
+                </Link>
+
+                <p>
+                    &copy; {new Date().getFullYear()} Event-ish, Inc. || All rights reserved
+                </p>
+            </div>
+        </footer>
     )
 }
--- components/navitems/NavItems.tsx
+++ components/navitems/NavItems.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+"use client"
+
+import { headerLinks } from '@/constants';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react';
 
 export default function NavItems() {
+    // Use pathname from next navigation to allow
+    // unique styling of the currently active path/link
+    const pathName = usePathname();
+
     return (
-        <div>
-            Nav-items
-        </div>
+        <ul className='flex md:flex-between w-full flex-col items-start gap-5 md:flex-row'>
+            {/* Map through array of links and render
+                all at once as a navitems compnent */}
+            {headerLinks.map((link) => {
+                // Set isActive if we're currently on pathname
+                const isActive = pathName === link.route;
+
+                return (
+                    <li key={link.route} className={`${isActive && 'text-primary-500'} flex-center p-medium-16 whitespace-nowrap`} >
+                        <Link href={link.route}>{link.label}</Link>
+                    </li>
+                )
+            })}
+        </ul >
     )
 }
